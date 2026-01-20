0d8c2aedcf4d7ad1a627b8edfef5185e0093c685
trucar00 <98335830+trucar00@users.noreply.github.com>
2026-01-20 11:28:25 +0100

    Dropped unnecessary columns

--- extract_capture_data.py
+++ extract_capture_data.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv("Data/fangstdata_2024.csv", sep=";", nrows=100, encoding="utf-8", decimal=",")
+df = pd.read_csv("Data/fangstdata_2024.csv", sep=";", encoding="utf-8", decimal=",")
 
-# DROP all unnecessary columns
-# Save new csv to look at
+df = df[["Fartøynavn", "Fartøy ID",  "Fartøytype (kode)", "Fartøynasjonalitet (kode)", "Største lengde", 
+         "Siste fangstdato", "Redskap", "Redskap - gruppe", "Hovedområde (kode)", "Hovedområde", "Lon (hovedområde)", "Lat (hovedområde)", 
+         "Nord/sør for 62 grader nord", "Landingsdato", "Art FAO", "Bruttovekt"]]
 
-print(df.head())
+df["Fartøy ID"] = df["Fartøy ID"].astype("Int64").astype("string")
+
+df.to_csv("Data/fangstdata_2024_dropped.csv", index=False)
+
+print(df.info())
 print(df.shape)
 print(df.columns)

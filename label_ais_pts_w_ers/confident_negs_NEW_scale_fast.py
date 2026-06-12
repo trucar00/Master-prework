7a7d57792d853c3005a57fb65775ee4cfe46a0f3
Truls Carlson <trulsc@ntnu.no>
2026-06-12 16:16:42 +0200

    conf 2025 new rules

--- label_ais_pts_w_ers/confident_negs_NEW_scale_fast.py
+++ label_ais_pts_w_ers/confident_negs_NEW_scale_fast.py
@@ -433,11 +433,10 @@ ALLOWED_LIST = [["Not", "no_fishing"], ["Trål", "no_fishing"], ["Krokredskap", 
 
 N_CLUSTERS = 2
 
-
 def main3():
     #path = "sub_labels/ais_ers_sub_labels_"
     path = "new_duration_limits/ais_ers_labels_"
-    for year in range(2024, 2024+1):
+    for year in range(2025, 2025+1):
         for start in range(1, 12+1, 3):   # starts at 1 and 
             dfs = []
             for i in range(start, start + 3):
